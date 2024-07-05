fa827f90fd7ca274a23e7ad3d6f92dbcd59ac1f5
MaVaVe <141180647+MaVaVe@users.noreply.github.com>
2024-07-05 12:43:04 -0600

    feat(location): real time actualization (#1)

--- pages/MapPage.tsx
+++ pages/MapPage.tsx
@@ -1,12 +1,23 @@
 import React, { useEffect, useState } from 'react';
-import MapView, {Marker} from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
 import { ActivityIndicator, StyleSheet, View } from 'react-native';
-import { LocationObject, getCurrentPositionAsync, requestForegroundPermissionsAsync, watchPositionAsync } from 'expo-location';
+import { Accuracy, LocationObject, LocationOptions, getCurrentPositionAsync, requestForegroundPermissionsAsync, watchPositionAsync } from 'expo-location';
+import { Socket, io } from 'socket.io-client';
+import * as Crypto from 'expo-crypto';
 
-
+const userId = Crypto.randomUUID()
 export default function MapPage() {
   const [currentLocation, setCurrentLocation] = useState<LocationObject>(null as any)
+  const [location, setLocation] = useState<LocationObject>(null as any)
   const [loading, setLoading] = useState(true)
+  const [socket, setSocket] = useState<Socket<any>>(undefined as any);
+  const [locations, setLocations] = useState<LocationObject[]>([])
+  // Replace with your server address
+
+  useEffect(() => {
+    const _socket = io('http://172.20.10.4:3000');
+    setSocket(_socket)
+  }, []);
 
   useEffect(() => {
     (async () => {
@@ -18,16 +29,29 @@ export default function MapPage() {
       await startTrace()
     })();
   }, []);
-  /*useEffect(() => {
-      if (socket) {
-          socket.emit('location actualization', { ...location.coords, ...{ id:USER_ID } });
-      }
-  }, [location]);*/
-  function handlePositionUpdate(location: LocationObject) {
 
+  useEffect(() => {
+    if(socket){
+        socket.on('location', (location:LocationObject):void => {
+          
+            setLocations((prevlocations) => {
+              const filteredLocations = prevlocations.filter(_location=>_location.id!==location.id)
+              return[...filteredLocations, location]});
+        })
+    }
+}, [socket]);
+
+  useEffect(() => {
+    if (socket) {
+      socket.emit('location', { ...location,...{["id"]:userId}});
+    }
+  }, [location]);
+  function handlePositionUpdate(_location: LocationObject) {
+    setLocation(_location)
   }
   async function startTrace() {
     const _location = await getCurrentPositionAsync()
+    const options:LocationOptions = {accuracy:Accuracy.Highest,distanceInterval:1}
     watchPositionAsync({}, handlePositionUpdate)
     setCurrentLocation(_location)
     setLoading(false)
@@ -44,11 +68,15 @@ export default function MapPage() {
         latitudeDelta: 0.0022,
         longitudeDelta: 0.0021
       }}>
-        <Marker
-          coordinate={{ latitude: currentLocation.coords.latitude, longitude:currentLocation.coords.longitude }}
-          image={require('../assets/favicon.png')}
-        />
-        </MapView>
+        {
+          locations.map(_location=>
+          <Marker
+            key={_location.id}
+            coordinate={{ latitude: _location.coords.latitude, longitude: _location.coords.longitude }}
+            image={require('../assets/favicon.png')}
+          />)
+        }
+      </MapView>
     </View>
   );
 }
